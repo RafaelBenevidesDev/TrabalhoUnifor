1eb240cdc8c6bd6828d4280ffb2e24bdc17e6059
Rafael Benevides <rafaelbenevidesdev@gmail.com>
2026-04-23 10:43:27 -0300

    Questao3AV2.js

--- questao3.js
+++ questao3.js
@@ -1,93 +1,36 @@
-let nomePassageiro = prompt("Digite seu nome: ");
-console.log("1 - Adulto; 2 - Estudante; 3 - Idoso; 4 - Criança menor de 5 anos");
-
-let categoria = parseInt(prompt("Digite o número correspondente a sua categoria: "));
-
-console.log("Destino A: R$45,00; Destino B: R$72,00; Destino C: R$110,00; Destino D: R$138,00");
-
-let destino = prompt("Digite a letra correspondente ao seu destino: ").toLowerCase();
-
-let qntdPassagens = parseInt(prompt("Digite a quantidade de passagens: "));
-let desconto;
-let valorBase;
-
-switch (categoria) {
-  case 1:
-    desconto = 0;
-    console.log("Sem desconto");
-    break;
-
-  case 2:
-    desconto = 50;
-    console.log("Desconto de 50%");
-    break;
-
-  case 3:
-    desconto = 50;
-    console.log("Desconto de 50%");
-    break;
-
-  case 4:
-    desconto = 100;
-    console.log("Passagem gratuita");
-    break;
-
-  default:
-    console.log("Categoria inválida");
-    break;
+let operaçao = 4
+let saldo = 1000
+let n = 0
+let valor = 0
+let totalOp = 0
+while(operaçao != n){
+    operaçao = prompt("=== MENU ===\n 1- Depósito\n 2- Saque\n 3- Consulta \n 0- Sair\n")
+    switch(operaçao){
+        case "1":
+            valor = parseFloat(prompt("Escolha: 1\n Valor do depósito: "));
+            saldo = saldo + valor
+            console.log("Depósito realizado. Saldo atual: ",(saldo.toFixed(2))) ;
+            totalOp++
+            break;
+        case "2":
+           valor = parseFloat(prompt("Escolha: 2\n Valor do saque: "));
+                if (valor <= saldo) {
+                    saldo = saldo - valor; 
+                    totalOp++
+                    console.log("Saque realizado. Saldo restante: ",(saldo.toFixed(2)));
+                } else {
+                        console.log("Saldo insuficiente!");
+                }
+            break;
+        case "3":
+            console.log("Escolha: 3\n Saldo: ",(saldo.toFixed(2)))
+            totalOp++
+            break;
+        case "0":
+            break;
+        default:
+            console.log("ERROR: operação inválida.")
+            break;
+    }
 }
-
-switch (destino) {
-  case "a":
-    valorBase = 45;
-    console.log("Valor base de R$45,00");
-    break;
-
-  case "b":
-    valorBase = 72;
-    console.log("Valor base de R$72,00");
-    break;
-
-  case "c":
-    valorBase = 110;
-    console.log("Valor base de R$110,00");
-    break;
-
-  case "d":
-    valorBase = 138;
-    console.log("Valor base de R$138,00");
-    break;
-
-  default:
-    console.log("Destino inválido");
-    break;
-}
-
-let subTotal = (valorBase * qntdPassagens);
-let subFormatado = subTotal.toFixed(2);
-let valorDesconto = (subTotal * (desconto / 100));
-let descFormatado = valorDesconto.toFixed(2);
-let total = (subTotal - valorDesconto);
-let totalFormatado = total.toFixed(2)
-
-total < 100? console.log("PAGAMENTO À VISTA") : console.log("PARCELÁVEL EM ATÉ 3X SEM JUROS");
-
-switch (true) {
-  case (total == 0):
-    console.log("Embarque gratuito liberado.");
-    break;
-
-  case (total < 100):
-    console.log("Dirija-se ao guichê para pagamento.");
-    break;
-
-  case (total < 200):
-    console.log("Pagamento via terminal disponível.");
-    break;
-
-  case (total >= 200):
-    console.log("Atendimento prioritário no guichê VIP.");
-    break;
-}
-
-console.log(nomePassageiro, `Categoria: ${categoria};`, `Destino: ${destino};`, `Subtotal: R$${subFormatado};`, `Valor desconto: R$${descFormatado};`, `Total à pagar: R$${totalFormatado};`);
+console.log("Escolha: 0\n Saldo final: ",(saldo.toFixed(2)),"\n Total de operações: ",(totalOp))
